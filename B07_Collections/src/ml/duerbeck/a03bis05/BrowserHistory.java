8247443673907afa36fbd31d7b737fb62c1c5922
CC-Student <CC-Student@192.168.0.38>
2021-06-30 23:41:52 +0200

    Hausaufgabe nachtraeglich von vorgestern erledigt.

--- B07_Collections/src/ml/duerbeck/a03bis05/BrowserHistory.java
+++ B07_Collections/src/ml/duerbeck/a03bis05/BrowserHistory.java
@@ -4,80 +4,103 @@ package ml.duerbeck.a03bis05;
 //String Array mit 5, oder ArrayList als Attribut
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class BrowserHistory {
-	
-	//Aufgabe01
-	String[] history;
-	
+
+	// Aufgabe01
+	Deque<String> history;
+	Deque<String> save;
+
 	BrowserHistory() {
-		history = new String[5];
+		history = new ArrayDeque<>();
+		save = new ArrayDeque<>();
 	}
 
 	public static void main(String[] args) {
 		BrowserHistory h = new BrowserHistory();
+
+		// Aufgabe02
+		h.open("u1.com");
+		h.open("u2.com");
+		h.open("u3.com");
+		h.open("u4.com");
+		h.open("u5.com");
+		System.out.println(h.getCurrent());
+		// Aufgabe03
+		System.out.println("Aufgabe 3:");
+		h.open("u6.com");
+		System.out.println(h.getCurrent());
+
+		// Aufgabe04
+		System.out.println("Aufgabe 4:");
+		h.openPrevious();
+		h.openPrevious();
+		h.openPrevious();
+		h.openPrevious();
+
+		// Aufgabe05
+		System.out.println("Aufgabe 5:");
+		try {
+			h.openPrevious();
+		} catch (IllegalStateException e) {
+			e.printStackTrace(); //getMessage funktioniert nicht
+		}
+		System.out.println( h.getCurrent() );
+
+		System.out.println("Aufgabe 6:");
+		h.openNext();
+		h.openNext();
+		h.openNext();
+		h.openNext();
 		
-	//Aufgabe02
-		 h.open("u1.com");
-		 h.open("u2.com");
-		 h.open("u3.com");
-		 h.open("u4.com");
-		 h.open("u5.com");
-//		 System.out.println(h.getCurrent());
-	//Aufgabe03	 
-		 h.open("u6.com");
-//		 System.out.println( h.getCurrent() );
-		 
-	//Aufgabe04	 
-		 h.openPrevious();
-		 h.openPrevious();
-		 h.openPrevious();
-		 h.openPrevious();
-		 h.openPrevious();
-//		 System.out.println( h.getCurrent() );
-	//Aufgabe05	 
-	  try {
-		  h.openPrevious();
-	  } catch (RuntimeException e) {
-		  e.getMessage();
-		  }
-	//  System.out.println( h.getCurrent() );
-	 
-		System.out.println("Aufgabe06");
-		h.openNext();
-		h.openNext();
-		h.openNext();
-		h.openNext();
-	//	System.out.println( h.getCurrent() );
+		System.out.println("Aufgabe 7:");
+		try {
+			h.openNext();
+		} catch (IllegalStateException e) {
+			e.printStackTrace(); //getMessage funktioniert nicht
+		}
+		System.out.println( h.getCurrent() ); // Leider wird hier u2.com, und nicht u6.com angezeigt.
 
 	}
 	
-	//Aufgabe06
+
+	// Aufgabe06 und 07
 	private void openNext() {
-	//	history.removeFirst();
+		if (history.size() > 4) {
+			throw new IllegalStateException("Next History is empty!");
+		} else {
+			this.history.addFirst(this.save.getFirst());
+			this.save.remove();
+			System.out.println(this.history.getFirst());
+		}
 	}
 
-	//Aufgabe04 u 05
-	private void openPrevious() throws RuntimeException {
-		List<String> list = Arrays.asList(history);
-		Deque<String> deque = new ArrayDeque<>(list);
-		deque.removeLast();
+	// Aufgabe04 u 05
+	private void openPrevious() throws IllegalStateException {
+		if (history.size() < 2) {
+			throw new IllegalStateException("Previous History is empty!");
+		} else {
+			this.save.addFirst(this.history.getLast());
+			this.history.removeLast();
+			System.out.println(this.history.getLast());
+		}
 	}
 
-	//Aufgabe02
-//	private String getCurrent() { 
-//		return history.getLast();
-//	}
+	// Aufgabe02
+	private String getCurrent() {
+		return this.history.getLast();
+	}
 
 	private void open(String string) {
+		if (this.history.size() < 5) {
+			this.history.offer(string);
+		} else {
+			this.history.remove(); // wieso geht poll nicht?
+			this.history.offer(string);
+		}
 
-		//history.offer(string);
 	}
-}
 
+}
